52c434807bbaa1002d78d7a9b27e90ee795bfc51
AndreyCorpse28090 <280990corpse@gmail.com>
2024-04-30 20:54:53 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace dictionary_examen_Bukov
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameTitle = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // nameTitle
+            // 
+            this.nameTitle.AutoSize = true;
+            this.nameTitle.Font = new System.Drawing.Font("Arial", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.nameTitle.ForeColor = System.Drawing.Color.Blue;
+            this.nameTitle.Location = new System.Drawing.Point(99, 58);
+            this.nameTitle.Name = "nameTitle";
+            this.nameTitle.Size = new System.Drawing.Size(152, 39);
+            this.nameTitle.TabIndex = 0;
+            this.nameTitle.Text = "Словари";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 462);
+            this.Controls.Add(this.nameTitle);
+            this.Name = "MainForm";
+            this.Text = "Словарь";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label nameTitle;
+    }
+}
+
